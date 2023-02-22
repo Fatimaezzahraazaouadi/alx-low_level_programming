f92aedcc4741ad097b30e67069afca77b72f2d18
Fatimaezzahraazaouadi <Fatimaezzahraazaouadi@gmail.com>
2023-02-22 08:49:43 -0800

    0-main.c, main.h

--- 0x03-debugging/postitive_or_negative.c
+++ 0x03-debugging/postitive_or_negative.c
@@ -1,14 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
+#include "main.h"
 
-/* betty style doc for function main goes there */
-int main(void)
+/**
+ * positive_or_negative - checks if given number is + , - or 0
+ * @i: number to be checked
+ * Return: nothing on success
+ */
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+if (i > 0)
+{
+printf("%d is positive\n" ,i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n" ,i);
+}
+else 
+{
+printf("%d is zero\n" ,i);
+}
 }
